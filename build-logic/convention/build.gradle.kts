cf25b9f07a0cace7a694bc69d29114c368a0905d
lnsantos <lucas.nepomuceno1999@gmail.com>
2023-01-25 23:59:01 -0300

    feat: created empty module project build-logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { `kotlin-dsl` }
+
+group = "com.lnsantos.monfunetwork.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
